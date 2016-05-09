16bfad8c11df3643e02dde516394cefd833c22a3
Stewart Yu <stewartyu@gmail.com>
2016-05-09 09:49:57 -0400

    Increase code coverage for accordion tests

--- src/accordion/Accordion.js
+++ src/accordion/Accordion.js
@@ -23,20 +23,15 @@ export const Accordion = {
 
     [...this.accordionSections].forEach((el) => {
       let trigger = el.querySelector(this.accordionSectionTrigger);
+      let sectionContent = el.querySelector(this.accordionSectionContent);
 
       trigger.setAttribute(`touch-action`, `none`);
-      el.setAttribute(`aria-expanded`, false)
+      el.setAttribute(`aria-expanded`, false);
 
       trigger.addEventListener(`pointerup`, (e) => {
         e.stopPropagation();
 
-        let sectionContent = el.querySelector(this.accordionSectionContent);
-
-        if (el.classList.contains(this.openClass)) {
-          this.closeAccordionSection(el, sectionContent);
-        } else {
-          this.openAccordionSection(el, sectionContent);
-        }
+        this.toggleAccordionSection(el, sectionContent);
       });
     });
   },
@@ -61,5 +56,18 @@ export const Accordion = {
     section.setAttribute(`aria-expanded`, false)
     section.classList.remove(this.openClass);
     Velocity(sectionContent, `slideUp`, this.transitionDuration);
+  },
+
+  /**
+   * Toggles the given Accordion section
+   * @param {object} section DOM element to toggle state class on
+   * @returns {void}
+  */
+  toggleAccordionSection(section, sectionContent) {
+    if (section.classList.contains(this.openClass)) {
+      this.closeAccordionSection(section, sectionContent);
+    } else {
+      this.openAccordionSection(section, sectionContent);
+    }
   },
 };
