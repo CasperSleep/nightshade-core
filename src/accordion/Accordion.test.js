be11406aaa4c8882febd3a3795e973213bc3c4eb
Stewart Yu <stewartyu@gmail.com>
2016-05-10 11:44:49 -0400

    Switch back to HTML fixtures to fix tests for now

--- src/accordion/Accordion.test.js
+++ src/accordion/Accordion.test.js
@@ -2,63 +2,60 @@ import assert from 'assert';
 
 import { Accordion } from './Accordion';
 
-import { env } from '../../nunjucks-helper.js';
+// import { env } from '../../nunjucks-helper.js';
 
 let accordion;
 
 const isHidden = (el) => {
-  let style = window.getComputedStyle(el);
+  const style = window.getComputedStyle(el);
 
-  return (style.display === 'none');
+  return (style.display === `none`);
 };
 
-describe('Accordion', () => {
+describe(`Accordion`, () => {
   before(() => {
-    fixture.setBase('src');
-
+    fixture.setBase(`src`);
   });
 
   beforeEach(() => {
-    console.log(env.render('accordion/Accordion.test.html'));
-    //accordion = fixture.load(env.render('accordion/Accordion.test.html'));
-    //console.log(accordion);
+    accordion = fixture.load(`accordion/Accordion.test.html`);
 
-    document.body.insertAdjacentHTML('afterbegin', accordion);
+    document.body.insertAdjacentHTML(`afterbegin`, accordion);
   });
 
   afterEach(() => {
-    accordion = document.querySelector('#accordion');
+    accordion = document.querySelector(`#accordion`);
     accordion.parentNode.removeChild(accordion);
   });
 
-  it('should initialize', () => {
-    Accordion.init('#accordion');
+  it(`should initialize`, () => {
+    Accordion.init(`#accordion`);
 
     assert(Accordion.accordion);
     assert.equal(Accordion.transitionDuration, 300);
   });
 
-  it('should initialize with custom transitionDuration', () => {
-    Accordion.init('#accordion', 100);
+  it(`should initialize with custom transitionDuration`, () => {
+    Accordion.init(`#accordion`, 100);
 
     assert.equal(Accordion.transitionDuration, 100);
-  })
+  });
 
-  it('should initialize with visible sectionContent', () => {
-    Accordion.init('#accordion');
+  it(`should initialize with visible sectionContent`, () => {
+    Accordion.init(`#accordion`);
 
-    const sectionContents = document.querySelectorAll('.js-accordion-section-content');
+    const sectionContents = document.querySelectorAll(`.js-accordion-section-content`);
 
-    for (var i = 0; i < sectionContents.length; i++) {
+    for (let i = 0; i < sectionContents.length; i++) {
       assert(!isHidden(sectionContents[i]));
     }
   });
 
-  it('can toggle accordion section', (done) => {
-    Accordion.init('#accordion', 0);
+  it(`can toggle accordion section`, (done) => {
+    Accordion.init(`#accordion`, 0);
 
-    let section = document.querySelector('.js-accordion-section');
-    let sectionContent = document.querySelector('.js-accordion-section-content');
+    const section = document.querySelector(`.js-accordion-section`);
+    const sectionContent = document.querySelector(`.js-accordion-section-content`);
 
     // close the accordion
     Accordion.toggleAccordionSection(section, sectionContent);
